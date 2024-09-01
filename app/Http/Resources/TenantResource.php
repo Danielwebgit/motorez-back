8a9b3cfc0f3ded5350afda3e448b6498851c5d43
Danielwebgit <nielferas@gmail.com>
2024-09-01 20:56:54 -0300

    Removed unnecessary field

--- app/Http/Resources/TenantResource.php
+++ app/Http/Resources/TenantResource.php
@@ -19,7 +19,6 @@ class TenantResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'domain' => 'http://'.$this->domain,
-            'database' => $this->database,
             'tenancy_db_name' => $this->tenancy_db_name,
             'created_at' => (new DateTime($this->created_at))->format('d-m-Y'),
             'updated_at' => (new DateTime($this->updated_at))->format('d-m-Y'),
